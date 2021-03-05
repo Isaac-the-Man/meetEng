af19cccce099a7af2770260e28e25ce792835041
Maxwell Zhao <thepiercingarrow@protonmail.com>
2021-03-04 19:29:08 -0500

    some docker stuff

---
+++ backend/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const config = require('./config.json');
+
+const express = require('express');
+
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(config.PORT, config.HOST);
+console.log(`Running on http://${config.HOST}:${config.PORT}`);
